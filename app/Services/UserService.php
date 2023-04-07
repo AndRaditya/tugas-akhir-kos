0b401990184ab43c4ebaed5a75686d428c0b61ed
Andreas Raditya Aryatama <raditya.aryatama@gmail.com>
2023-04-07 12:57:54 +0700

    user spacing

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -10,7 +10,7 @@ class UserService
     private $userRepository;
 
     public function __construct(UserRepository $userRepository){
-      $this->userRepository = $userRepository;
+        $this->userRepository = $userRepository;
     }
 
     public function getAll(){
